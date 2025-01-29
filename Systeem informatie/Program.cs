684666a952e2334b374dd00c7ee1c35ed2af780e
Niels Knockaert <knockaertniels@leerling.sint-rembert.be>
2025-01-29 10:05:10 +0100

    opdracht Systeem_informatie

--- Systeem informatie/Program.cs
+++ Systeem informatie/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int ProcessorCount = 8;
+            string WorkingSet = "23.00 MB (0.02 GB)";
+            string MachineName = "damsPowahPC";
+            string is64OperatingSystem = "True";
+            string UserName = "admin";
+            Console.WriteLine($"Systeeminformatie voor {UserName} op {MachineName}:\r\n---------------------------------------------------\r\n    Aantal processors: {ProcessorCount}\r\n    64-bit besturingssysteem: {is64OperatingSystem}\r\n    Huidige geheugengebruik: {WorkingSet}\r\n---------------------------------------------------");
         }
     }
 }

6ad76d0d99a29388203875ab4dd7f4210c2cd955
Niels Knockaert <knockaertniels@leerling.sint-rembert.be>
2025-02-06 09:08:38 +0100

    6 02 Niels K

--- Systeem informatie/Program.cs
+++ Systeem informatie/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-            int ProcessorCount = 8;
-            string WorkingSet = "23.00 MB (0.02 GB)";
-            string MachineName = "damsPowahPC";
-            string is64OperatingSystem = "True";
-            string UserName = "admin";
+            int ProcessorCount = Environment.ProcessorCount;
+            long WorkingSet = Environment.WorkingSet;
+            string MachineName = Environment.MachineName;
+            bool is64OperatingSystem = Environment.Is64BitOperatingSystem;
+            string UserName = Environment.UserName;
             Console.WriteLine($"Systeeminformatie voor {UserName} op {MachineName}:\r\n---------------------------------------------------\r\n    Aantal processors: {ProcessorCount}\r\n    64-bit besturingssysteem: {is64OperatingSystem}\r\n    Huidige geheugengebruik: {WorkingSet}\r\n---------------------------------------------------");
         }
     }
